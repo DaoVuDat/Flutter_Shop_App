e22fe1591fb59c0dde6e49ee9739e2565b48e804
Dao Vu Dat <dat.daovu@gmail.com>
2019-12-08 23:55:52 +0700

    finish section http request and add FutureBuilder

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -43,18 +43,7 @@ class CartScreen extends StatelessWidget {
                     ),
                     backgroundColor: Theme.of(context).primaryColor,
                   ),
-                  FlatButton(
-                    child: Text("ORDER NOW"),
-                    onPressed: cart.itemCount == 0 ? null : 
-                    () {
-                      Provider.of<Orders>(context, listen: false).addOrder(
-                        cart.items.values.toList(),
-                        cart.totalAmount,
-                      );
-                      cart.clear();
-                    },
-                    textColor: Theme.of(context).primaryColor,
-                  )
+                  OrderButton(cart: cart)
                 ],
               ),
             ),
@@ -75,6 +64,47 @@ class CartScreen extends StatelessWidget {
           )
         ],
       ),
+    );
+  }
+}
+
+class OrderButton extends StatefulWidget {
+  const OrderButton({
+    Key key,
+    @required this.cart,
+  }) : super(key: key);
+
+  final Cart cart;
+
+  @override
+  _OrderButtonState createState() => _OrderButtonState();
+}
+
+class _OrderButtonState extends State<OrderButton> {
+  var _isLoading = false;
+  @override
+  Widget build(BuildContext context) {
+    return FlatButton(
+      child: _isLoading ? CircularProgressIndicator() : Text("ORDER NOW"),
+      onPressed: (widget.cart.itemCount <= 0 || _isLoading) ? null : 
+      () async {
+        // rebuild the widget with isLoading = true
+        setState(() {
+          _isLoading = true;
+        });
+        
+        await Provider.of<Orders>(context, listen: false).addOrder(
+          widget.cart.items.values.toList(),
+          widget.cart.totalAmount,
+        );
+
+        // rebuild the widget with isLoading = false AFTER await
+        setState(() {
+          _isLoading = false;
+        });
+        widget.cart.clear();
+      },
+      textColor: Theme.of(context).primaryColor,
     );
   }
 }
--- lib/screens/order_screen.dart
+++ lib/screens/order_screen.dart
@@ -5,22 +5,77 @@ import '../providers/order.dart' show Orders;
 
 import '../widgets/order_item.dart';
 import '../widgets/app_draw.dart';
+
 class OrderScreen extends StatelessWidget {
-  static const routeName ="/orders";
+  static const routeName = "/orders";
+  // var _isLoading = false;
+  // var _isInit = true;
+
+  // @override
+  // void initState() {
+  //   // TODO: implement initState
+  //   super.initState();
+
+  //   // _isLoading = true;
+
+  //   // Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+  //   //   setState(() {
+  //   //     _isLoading = false;
+  //   //   });
+  //   // });
+  // }
+
+  // @override
+  // void didChangeDependencies() {
+  //   // TODO: implement didChangeDependencies
+  //   super.didChangeDependencies();
+  //   if (_isInit) {
+  //     _isInit = false;
+  //     setState(() {
+  //       _isLoading = true;
+  //     });
+  //     Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
+  //       setState(() {
+  //         _isLoading = false;
+  //       });
+  //     });
+  //   }
+  // }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
-
+    // final orderData = Provider.of<Orders>(context);
+    print('building orders');
     return Scaffold(
       appBar: AppBar(
         title: Text("Your Orders"),
       ),
       drawer: AppDraw(),
-      body: ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
-      ),
+      body: FutureBuilder(
+          future:
+              Provider.of<Orders>(context, listen: false).fetchAndSetOrders(),
+          builder: (ctx, dataSnapShot) {
+            if (dataSnapShot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            } else {
+              if (dataSnapShot.error != null) {
+                // error handler
+                return Center(
+                  child: Text('An error occured!'),
+                );
+              } else {
+                // this widget need data from Orders
+                return Consumer<Orders>(
+                  builder: (ctx, orderData, child) => ListView.builder(
+                    itemCount: orderData.orders.length,
+                    itemBuilder: (ctx, i) => OrderItem(orderData.orders[i]),
+                  ),
+                );
+              }
+            }
+          }),
     );
   }
 }
